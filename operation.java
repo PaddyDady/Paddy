b5950401c0b2ce650d1f83f71cb2a84dbebfcf00
PaddyDady <pratham9819@gmail.com>
2023-08-26 11:47:48 +0530

    First Java Code

---
+++ operation.java
@@ -0,0 +1,18 @@
+public class operation
+{
+    public static void main(String[] args) 
+    {
+        int a,b,sum,div,mul,sub;
+        a=100;
+        b=200;
+        sum=a+b;
+        div=b/a;
+        mul=a*b;
+        sub=a-b;
+        System.out.println("sum is\t"+sum );
+        System.out.println("div is\t"+div);
+        System.out.println("mul is\t"+mul);
+        System.out.println("sub is\t"+sub);
+    }
+    
+}
